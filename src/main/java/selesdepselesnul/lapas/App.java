380d02fc53b0200002e50afa4fcdc339d7ac29ce
selesdepselesnul <selesdepselesnul@gmail.com>
2015-09-07 09:28:22 +0700

    add maven dep

---
+++ src/main/java/selesdepselesnul/lapas/App.java
@@ -0,0 +1,13 @@
+package selesdepselesnul.lapas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "first commit!" );
+    }
+}
