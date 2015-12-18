91c4dd577a3dd66361ebceff74e7045af59a521d
selesdepselesnul <selesdepselesnul@gmail.com>
2015-12-18 16:04:05 +0700

    change course number pattern

--- src/main/java/livinglikelarry/lapgas/util/LapgasPattern.java
+++ src/main/java/livinglikelarry/lapgas/util/LapgasPattern.java
@@ -2,5 +2,5 @@ package livinglikelarry.lapgas.util;
 
 public class LapgasPattern {
 	public static final String STUDENT_NUMBER = "4115505\\d{7}"; 
-	public static final String COURSE_NUMBER = "TF-\\d{4}";
+	public static final String COURSE_NUMBER = "TIF\\d{4}";
 }
